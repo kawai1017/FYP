97308d1ecfad16501da99015dc91bc09433d9950
mervynckw <mervynchan2002@gmail.com>
2017-01-23 20:48:27 +0800

    appbar

--- app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intialpage);
+        setContentView(R.layout.intial_page);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
