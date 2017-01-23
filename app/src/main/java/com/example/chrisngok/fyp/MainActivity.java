a18ce3c97f89194ed46b474c2f260045a72472a1
kawai1017 <chrisngok@Chriss-MacBook-Air.local>
2017-01-23 20:49:40 +0800

    Chris

--- app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton ib;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
