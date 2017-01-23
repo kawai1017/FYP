a8680c691da90d15101e81a058ec2ef4a4abe4ab
kawai1017 <chrisngok@Chriss-MacBook-Air.local>
2017-01-24 01:10:07 +0800

    Chris

--- app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.chrisngok.fyp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+
+
         ib = (ImageButton) findViewById(R.id.button_hkmoh);
         ib.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,11 +33,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.appbar, menu);
         return true;
     }
+    /*@Override
+    public boolean onOptionsItemsSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.about:
+                aboutMenuItem();
+                break;
+            case R.id.action_settings:
+                settingsMenuItem();
+                break;
+            case R.id.exit:
+                exitMenuItem();
+                break;
+        }
+            return true;
+    }
+    private void aboutMenuItem(){
+        new AlertDialog.Builder(this)
+        .setTitle("About")
+        .setMessage("This is about message dialog")
+        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int whcih){
+            }
+        }).show();
+    }
+    private void settingsMenuItem(){
+        new AlertDialog.Builder(this)
+                .setTitle("Setting")
+                .setMessage("This is setting message dialog")
+        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int whcih){
+            }
+        }).show();
+    }
+    private void exitMenuItem(){
+        new AlertDialog.Builder(this)
+                .setTitle("exit")
+                .setMessage("This is exit message dialog")
+        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int whcih){
+            }
+        }).show();
+    }*/
 }
 
