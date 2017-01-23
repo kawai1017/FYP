8528e44e5f10ec4ae619c5a45d305f8789213b6c
mervynckw <mervynchan2002@gmail.com>
2017-01-23 21:34:26 +0800

    mervyn

--- app/src/main/java/com/example/chrisngok/fyp/HKMOH_Page1Activity.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKMOH_Page1Activity.java
@@ -1,12 +1,13 @@
 package com.example.chrisngok.fyp;
-
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
-public class HKMOH_Page1Activity extends Activity {
+public class HKMOH_Page1Activity extends AppCompatActivity {
 
     MediaPlayer mySound;
 
@@ -14,9 +15,15 @@ public class HKMOH_Page1Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hkmoh_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         mySound = MediaPlayer.create(this, R.raw.t_hot);
     }
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.appbar, menu);
+        return true;
+    }
     public void playSound(View view) {
         mySound.start();
     }
