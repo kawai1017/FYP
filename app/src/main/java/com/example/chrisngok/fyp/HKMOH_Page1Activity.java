06c76ebd90fa82da1cb913e7114eecc03c94aa98
spha0605 <spha0605@gmail.com>
2017-01-23 18:12:38 +0800

    sze

--- app/src/main/java/com/example/chrisngok/fyp/HKMOH_Page1Activity.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKMOH_Page1Activity.java
@@ -3,21 +3,19 @@ package com.example.chrisngok.fyp;
 import android.app.Activity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.app.Activity;
 
 public class HKMOH_Page1Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.hkmoh_main);
     }
 
     /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.intialPage, menu);
         return true;
     }*/
 }
--- app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/MainActivity.java
@@ -4,18 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
-import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton ib;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.intialPage);
 
         ib = (ImageButton) findViewById(R.id.button_hkmoh);
         ib.setOnClickListener(new View.OnClickListener() {
