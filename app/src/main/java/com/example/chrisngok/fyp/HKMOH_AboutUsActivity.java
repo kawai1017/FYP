605f9eede29308f4752a3af5b51a8d5746ed521d
spha0605 <spha0605@gmail.com>
2017-01-24 21:33:21 +0800

    add about_us page

--- app/src/main/java/com/example/chrisngok/fyp/HKMOH_AboutUsActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKMOH_AboutUsActivity.java
@@ -1,35 +1,18 @@
 package com.example.chrisngok.fyp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    ImageButton ib;
-
-    @Override
+public class HKMOH_AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intial_page);
-
+        setContentView(R.layout.hkmoh_about_us);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
-        ib = (ImageButton) findViewById(R.id.button_hkmoh);
-        ib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, HKMOH_MainActivity.class);
-                startActivity(myIntent);
-            }
-        });
     }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.appbar, menu);
--- app/src/main/java/com/example/chrisngok/fyp/HKMOH_MainActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKMOH_MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.chrisngok.fyp;
-import android.support.v7.app.AppCompatActivity;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 
-public class HKMOH_Page1Activity extends AppCompatActivity {
+public class HKMOH_MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +17,20 @@ public class HKMOH_Page1Activity extends AppCompatActivity {
         setContentView(R.layout.hkmoh_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        Button button_about_us = (Button) findViewById(R.id.about_us);
+        button_about_us.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent i=new Intent(HKMOH_MainActivity.this, HKMOH_AboutUsActivity.class);
+            startActivity(i);
+        }
+        });
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.appbar, menu);
         return true;
     }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.intialPage, menu);
-        return true;
-    }*/
 }
