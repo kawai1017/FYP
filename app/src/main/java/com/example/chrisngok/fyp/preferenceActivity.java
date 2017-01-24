b4b6143520ddbb86b3af46f81bb2900c96d87241
mervynckw <mervynchan2002@gmail.com>
2017-01-25 00:45:43 +0800

    push prferenceActivity.java

---
+++ app/src/main/java/com/example/chrisngok/fyp/preferenceActivity.java
@@ -0,0 +1,19 @@
+package com.example.chrisngok.fyp;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class preferenceActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    edit_perferences prefFragment = new edit_perferences() ;
+    transaction.add(android.R.id.content, prefFragment);
+    transaction.commit();
+}
+}
