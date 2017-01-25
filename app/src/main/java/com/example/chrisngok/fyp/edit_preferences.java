e39ff24c77d27a32a30e4ca3815f288e900ba577
mervynckw <mervynchan2002@gmail.com>
2017-01-26 06:27:04 +0800

    add change language script , padding to each imagebutton, Activity label to mainfest

--- app/src/main/java/com/example/chrisngok/fyp/edit_preferences.java
+++ app/src/main/java/com/example/chrisngok/fyp/edit_preferences.java
@@ -1,14 +1,55 @@
 package com.example.chrisngok.fyp;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+import java.util.Locale;
 
 
-
-public class edit_preferences extends PreferenceFragment {
+public class edit_preferences extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.editor_prefs);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+
+        if (key.equals("lang"))
+        {
+            // update language
+            Locale myLocale = new Locale(sharedPreferences.getString("lang",""));
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this.getContext(), MainActivity.class);
+            startActivity(refresh);
+
+        }
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 }
