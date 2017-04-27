c75d31c504af7ca26522cfe9f0fba3edcaeb8b66
spha0605 <spha0605@gmail.com>
2017-04-27 16:02:08 +0800

    code reformed

--- app/src/main/java/com/example/chrisngok/fyp/baseActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/baseActivity.java
@@ -2,23 +2,12 @@ package com.example.chrisngok.fyp;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -30,15 +19,17 @@ public abstract class BaseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    public void  setContentView(int layoutResID){
+    public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
         setUpToolBar();
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.appbar, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
@@ -63,13 +54,7 @@ public abstract class BaseActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
 
-    /*
-    protected void onDestroy(){
-        super.onDestroy();
-        //Kill myself
-        android.os.Process.killProcess(android.os.Process.myPid());
-    } */
+
 }
