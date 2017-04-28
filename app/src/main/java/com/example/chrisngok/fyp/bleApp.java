63668817a8b6afe5cfa9205a8ebce0f040e1740a
mervynckw <mervynchan2002@gmail.com>
2017-04-28 18:29:05 +0800

    add bluetooth scanner.

--- app/src/main/java/com/example/chrisngok/fyp/bleApp.java
+++ app/src/main/java/com/example/chrisngok/fyp/bleApp.java
@@ -1,20 +1,8 @@
 package com.example.chrisngok.fyp;
 
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 
-import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-import com.estimote.sdk.connection.internal.protocols.Operation;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public class bleApp extends Application {
 
