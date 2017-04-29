5a89446b401ae31f9783102ba4ff602ccee21f61
chrisngok <kawaingok@gmail.com>
2017-04-29 23:34:04 +0800

    Change the visiting Infromation XML style from 4 museum indivdual xml to 1 xml then use JAVA call in runtime. And Delete related related us XML. And add different value in string. Modified the manifest. Modified the java calss

--- app/src/main/java/com/example/chrisngok/fyp/HKHM_VisitingInformation.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKHM_VisitingInformation.java
@@ -1,54 +1,55 @@
 package com.example.chrisngok.fyp;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class HKMOH_VisitingInformation extends BaseActivity {
-    String startlang;
+public class HKHM_VisitingInformation extends BaseActivity {
+    String startLang;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visitinginformation);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        startlang = preferences.getString("lang", "");
+        startLang = preferences.getString("lang", "");
+
         ImageView img = (ImageView) findViewById(R.id.imageView3);
-        img.setImageResource(R.drawable.about_image);
+        img.setImageResource(R.drawable.frontpage_hkhm);
 
         TextView address_title = (TextView) findViewById(R.id.addr_title);
         address_title.setText(R.string.address);
         TextView address = (TextView) findViewById(R.id.addr);
-        address.setText(R.string.hkmoh_addr);
+        address.setText(R.string.hkhm_addr);
 
         TextView tel_title = (TextView) findViewById(R.id.tel_title);
         tel_title.setText(R.string.telephone);
         TextView tel = (TextView) findViewById(R.id.tel);
-        tel.setText(R.string.hkmoh_tel);
+        tel.setText(R.string.hkhm_tel);
 
         TextView fax_title = (TextView) findViewById(R.id.fax_title);
         fax_title.setText(R.string.fax);
         TextView fax = (TextView) findViewById(R.id.fax);
-        fax.setText(R.string.hkmoh_fax);
+        fax.setText(R.string.hkhm_fax);
 
         TextView email_title = (TextView) findViewById(R.id.email_title);
         email_title.setText(R.string.fax);
         TextView email = (TextView) findViewById(R.id.email);
-        email.setText(R.string.hkmoh_email);
+        email.setText(R.string.hkhm_email);
 
         TextView website_title = (TextView) findViewById(R.id.website_title);
         website_title.setText(R.string.website);
         TextView website1 = (TextView) findViewById(R.id.website1);
-        website1.setText(R.string.hkmoh_website1);
-
-        TextView website2 = (TextView) findViewById(R.id.website2);
-        website2.setText(R.string.hkmoh_website2);
+        website1.setText(R.string.hkhm_website1);
     }
+
 
     @Override
     protected void onResume() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String currentlang = preferences.getString("lang","");
-        if(!startlang.matches(currentlang)){
+        String currentLang = preferences.getString("lang", "");
+        if (!startLang.matches(currentLang)) {
             recreate();
         }
         super.onResume();
--- app/src/main/java/com/example/chrisngok/fyp/HKSpace_VisitingInformation.java
+++ app/src/main/java/com/example/chrisngok/fyp/HKSpace_VisitingInformation.java
@@ -1,54 +1,54 @@
 package com.example.chrisngok.fyp;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class HKMOH_VisitingInformation extends BaseActivity {
+public class HKSpace_VisitingInformation extends BaseActivity {
     String startlang;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visitinginformation);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         startlang = preferences.getString("lang", "");
+
         ImageView img = (ImageView) findViewById(R.id.imageView3);
-        img.setImageResource(R.drawable.about_image);
+        img.setImageResource(R.drawable.frontpage_hkspacemuseum);
 
         TextView address_title = (TextView) findViewById(R.id.addr_title);
         address_title.setText(R.string.address);
         TextView address = (TextView) findViewById(R.id.addr);
-        address.setText(R.string.hkmoh_addr);
+        address.setText(R.string.hkspace_addr);
 
         TextView tel_title = (TextView) findViewById(R.id.tel_title);
         tel_title.setText(R.string.telephone);
         TextView tel = (TextView) findViewById(R.id.tel);
-        tel.setText(R.string.hkmoh_tel);
+        tel.setText(R.string.hkspace_tel);
 
         TextView fax_title = (TextView) findViewById(R.id.fax_title);
         fax_title.setText(R.string.fax);
         TextView fax = (TextView) findViewById(R.id.fax);
-        fax.setText(R.string.hkmoh_fax);
+        fax.setText(R.string.hkspace_fax);
 
         TextView email_title = (TextView) findViewById(R.id.email_title);
         email_title.setText(R.string.fax);
         TextView email = (TextView) findViewById(R.id.email);
-        email.setText(R.string.hkmoh_email);
+        email.setText(R.string.hkspace_email);
 
         TextView website_title = (TextView) findViewById(R.id.website_title);
         website_title.setText(R.string.website);
         TextView website1 = (TextView) findViewById(R.id.website1);
-        website1.setText(R.string.hkmoh_website1);
-
-        TextView website2 = (TextView) findViewById(R.id.website2);
-        website2.setText(R.string.hkmoh_website2);
+        website1.setText(R.string.hkspace_website1);
     }
 
     @Override
     protected void onResume() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String currentlang = preferences.getString("lang","");
-        if(!startlang.matches(currentlang)){
+        String currentlang = preferences.getString("lang", "");
+        if (!startlang.matches(currentlang)) {
             recreate();
         }
         super.onResume();
