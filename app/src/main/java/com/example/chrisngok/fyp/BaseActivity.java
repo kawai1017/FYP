d9c207400b8182c59edf2d25083e9fd7a5d3b290
mervynckw <mervynchan2002@gmail.com>
2017-04-30 17:03:38 +0800

    Restyling the appbar.

--- app/src/main/java/com/example/chrisngok/fyp/BaseActivity.java
+++ app/src/main/java/com/example/chrisngok/fyp/BaseActivity.java
@@ -60,7 +60,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
 
     protected void killApp() {
-        super.onDestroy();
         //Kill myself
         android.os.Process.killProcess(android.os.Process.myPid());
     }
